8499f5d9f183eeab2a2112c130faed5c981d16ff
gdubon8806 <germandubon3@gmail.com>
2024-11-16 13:52:28 -0600

    BASIC template modeling data in UI tables

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const ADD_ENTRY_MODAL_CONTAINER = document.querySelector('.add-entry-modal-conta
 const DATE_ENTRY_TXT = document.querySelector('.date-entry-txt');
 const DESC_ENTRY_TXT = document.querySelector('.desc-entry-txt');
 const AMT_ENTRY_TXT = document.querySelector('.amt-entry-txt');
+const TYPE_ENTRY_SELECT = document.querySelector('.type-entry-select');
 
 
 //UI elements
@@ -37,44 +38,122 @@ function toggleModalEntry() {
 function addNewEntry() {
     ENTRIES.push(createNewEntry()); //updates state
 
-    updateTableEntries(); //updates UI
+    renderTableEntries(); //updates UI
 }
 
 function createNewEntry() {
     //first get current input data
-    let tempDATE_ENTRY_TXT =  DATE_ENTRY_TXT.value;
-    let tempDESC_ENTRY_TXT =  DESC_ENTRY_TXT.value;
-    let tempAMT_ENTRY_TXT =  AMT_ENTRY_TXT.value;
+    let tempDATE_ENTRY_TXT = DATE_ENTRY_TXT.value;
+    let tempDESC_ENTRY_TXT = DESC_ENTRY_TXT.value;
+    let tempAMT_ENTRY_TXT = AMT_ENTRY_TXT.value;
+    let tempTYPE_ENTRY_SELECT = TYPE_ENTRY_SELECT.value;
 
-    const newEntry = new Entry(tempDATE_ENTRY_TXT, tempDESC_ENTRY_TXT, tempAMT_ENTRY_TXT);
+    const newEntry = new Entry(tempDATE_ENTRY_TXT, tempDESC_ENTRY_TXT, tempAMT_ENTRY_TXT, tempTYPE_ENTRY_SELECT);
+
     return newEntry;
 }
 
-function updateTableEntries() {
-    let rowEntry = document.createElement('tr');
-    let tableDataDesc = document.createElement('td');
-    let tableDataAmt = document.createElement('td');
-    let tableDataDate = document.createElement('td');
-    
-    let newEntryToShow = ENTRIES[ENTRIES.length - 1];
+const ENTRIES_CONTAINER = document.querySelector('.entries-container')
 
-    tableDataDesc.innerHTML = newEntryToShow.description;
-    tableDataAmt.innerHTML = newEntryToShow.amount;
-    tableDataDate.innerHTML = newEntryToShow.date;
+function renderTableEntries() {
+    ENTRIES_CONTAINER.innerHTML = ""; //restarts entries container
 
-    rowEntry.appendChild(tableDataDesc);
-    rowEntry.appendChild(tableDataAmt);
-    rowEntry.appendChild(tableDataDate);
+    let copy_entries = ENTRIES.slice(); //copy of array entries 
 
-    TABLE_ENTRIES.appendChild(rowEntry);
+    //grouped by date entries 
+    const entries_grouped_by_dates = Object.groupBy(copy_entries, ({ date }) => date);
+
+    //iterate over this object and render data in table with template
+    for (let entryDate in entries_grouped_by_dates) {
+        let template = document.querySelector(".day-entries-template");
+        let content = template.content.cloneNode(true);
+
+        let dateToFormat = entryDate + "T00:00:00";
+        const fecha = new Date(dateToFormat);
+
+        // Opciones para el formato deseado
+        const opciones = {
+            weekday: 'long', // Día de la semana completo
+            day: 'numeric', // Día del mes
+            month: 'long' // Nombre completo del mes
+        };
+
+        // Formatear la fecha en español
+        const formateador = new Intl.DateTimeFormat('es-HN', opciones);
+        const fechaFormateada = formateador.format(fecha);
+
+        //primero poner la fecha de encabezado
+        content.querySelector(".date-header").innerHTML = fechaFormateada.charAt(0).toUpperCase() + fechaFormateada.slice(1);;
+
+        //iterar sobre entries para egresos
+        let dateEntries = entries_grouped_by_dates[entryDate];
+
+
+        //solo spendings
+        let spendingsDateEntries = dateEntries.filter((entry) => {
+            if (entry.type == "Withdrawal") return true;
+        })
+
+        if (spendingsDateEntries.length > 0) {
+            for (let i = 0; i < spendingsDateEntries.length; i++) {
+                let rowEntry = document.createElement('tr');
+                let tableDataDesc = document.createElement('td');
+                let tableDataAmt = document.createElement('td');
+
+                tableDataDesc.innerHTML = spendingsDateEntries[i].description;
+                tableDataAmt.innerHTML = 'L. ' + spendingsDateEntries[i].amount;
+
+                rowEntry.appendChild(tableDataDesc);
+                rowEntry.appendChild(tableDataAmt);
+
+                content.querySelector(".spending-table-entries").append(rowEntry);
+            }
+
+        } else {
+            let rowEntry = document.createElement('tr');
+            rowEntry.innerHTML = "No hay entries"
+            content.querySelector(".spending-table-entries").append(rowEntry);
+        }
+
+        //iterar sobre entries para ingresos
+        //solo spendings
+        let depositsDateEntries = dateEntries.filter((entry) => {
+            if (entry.type == "Deposit") return true;
+        })
+
+        if (depositsDateEntries.length > 0) {
+
+            for (let i = 0; i < depositsDateEntries.length; i++) {
+                let rowEntry = document.createElement('tr');
+                let tableDataDesc = document.createElement('td');
+                let tableDataAmt = document.createElement('td');
+
+                tableDataDesc.innerHTML = depositsDateEntries[i].description;
+                tableDataAmt.innerHTML = 'L. ' + depositsDateEntries[i].amount;
+
+                rowEntry.appendChild(tableDataDesc);
+                rowEntry.appendChild(tableDataAmt);
+
+                content.querySelector(".income-table-entries").append(rowEntry);
+            }
+        } else {
+            let rowEntry = document.createElement('tr');
+            rowEntry.innerHTML = "No hay entries"
+            content.querySelector(".income-table-entries").append(rowEntry);
+        }
+
+
+        document.querySelector(".entries-container").append(content);
+    }
 
 }
 
 
 class Entry {
-    constructor(date, description, amount) {
+    constructor(date, description, amount, type) {
         this.date = date;
         this.description = description;
-        this .amount = amount;
-    }  
+        this.amount = amount;
+        this.type = type
+    }
 }
